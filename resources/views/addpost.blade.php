32c191b98f2f097e3846cebf3fda152593a8c9a6
Muhammad Hashim <mhashimmgr36@gmail.com>
2024-07-23 23:24:07 +0500

    nothing

--- resources/views/addpost.blade.php
+++ resources/views/addpost.blade.php
@@ -81,12 +81,3 @@
     </div>
 </div>
 @endsection
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
-<script>
-  tinymce.init({
-    selector: 'textarea',
-    plugins: 'advlist autolink lists link image charmap preview anchor textcolor',
-    toolbar: 'undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat',
-    menubar: false
-  });
-</script>
